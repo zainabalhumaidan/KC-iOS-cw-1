8c4929425c2226641bfdfd57e3242c2c4932e795
zainabalhumaidan <114605180+zainabalhumaidan@users.noreply.github.com>
2022-10-05 19:11:14 +0300

    firstry

---
+++ firstTry/firstTry/firstTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstTryApp.swift
+//  firstTry
+//
+//  Created by Zainab Alhumaidan on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct firstTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
